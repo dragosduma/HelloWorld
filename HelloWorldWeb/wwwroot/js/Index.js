e9cbdb77ae09503c608a757f25b9fee94bdb7344
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-03 11:48:20 +0300

    Change team member list implementation

--- HelloWorldWeb/wwwroot/js/Index.js
+++ HelloWorldWeb/wwwroot/js/Index.js
@@ -2,9 +2,7 @@ $(document).ready(function () {
 
     $("#createButton").click(function () {
         var newcomerName = $("#nameField").val();
-
         $("#teamMembers").append(`<li>${newcomerName}</li>`);
-
         $("#nameField").val("");
     })
 });
