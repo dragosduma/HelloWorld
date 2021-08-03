8111cdc9736586215710efc83fb694f41b0d77c4
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-03 10:35:21 +0300

    Remake button

---
+++ HelloWorldWeb/wwwroot/js/Index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $("#createButton").click(function () {
+        var newcomerName = $("#nameField").val();
+
+        $("#teamMembers").append(`<li>${newcomerName}</li>`);
+
+        $("#nameField").val("");
+    })
+});
--- HelloWorldWeb/wwwroot/js/site.js
+++ HelloWorldWeb/wwwroot/js/site.js
@@ -1,15 +1,5 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-    $(document).ready(function () {
-
-        $("#createButton").click(function () {
-            var newcomerName = $("#nameField").val();
-
-            $("#teamMembers").append(`<li>${newcomerName}</li>`);
-
-            $("#nameField").val("");
-        })
-    });
  
 // Write your JavaScript code.
