6d2d583c2674841d368ca4f0d57f8c61948b0f64
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-04 11:46:09 +0300

    Added ajax to index

--- HelloWorldWeb/wwwroot/js/Index.js
+++ HelloWorldWeb/wwwroot/js/Index.js
@@ -2,7 +2,17 @@ $(document).ready(function () {
 
     $("#createButton").click(function () {
         var newcomerName = $("#nameField").val();
-        $("#teamMembers").append(`<li>${newcomerName}</li>`);
-        $("#nameField").val("");
+      
+        $.ajax({
+            url: "https://localhost:44313/Home/AddTeamMember",
+            method: "POST",
+            data: {
+                "name": newcomerName
+            },
+            success: function (result) {
+                $("#teamMembers").append(`<li>${newcomerName}</li>`);
+                $("#nameField").val("");
+            }
+        })  
     })
 });
