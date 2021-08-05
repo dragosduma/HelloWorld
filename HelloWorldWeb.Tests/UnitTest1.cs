820d6ac0a6fc0ee52bb9f7366fcac8ddfe062c01
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-05 12:28:53 +0300

    Added unit test for remove

--- HelloWorldWeb.Tests/UnitTest1.cs
+++ HelloWorldWeb.Tests/UnitTest1.cs
@@ -16,8 +16,21 @@ namespace HelloWorldWeb.Tests
             teamService.AddTeamMember("intern");
 
             // Assert
-            Assert.Equal(7, teamService.GetTeamInfo().TeamMembers.Count);
-            
+            Assert.Equal(7, teamService.GetTeamInfo().TeamMembers.Count);           
+        }
+
+        [Fact]
+        public void RemoveMemberFromTheTeamAfterAdding()
+        {
+            // Assume
+            TeamService teamService = new TeamService();
+
+            // Act
+            teamService.AddTeamMember("intern");
+            teamService.RemoveMember(2);
+
+            // Assert
+            Assert.Equal(6, teamService.GetTeamInfo().TeamMembers.Count);
         }
     }
 }
