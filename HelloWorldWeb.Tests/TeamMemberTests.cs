cda9a666735ad8108ab6efe08ddb0050bef5a10b
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-11 12:03:05 +0300

    Added unit test to check Id

--- HelloWorldWeb.Tests/TeamMemberTests.cs
+++ HelloWorldWeb.Tests/TeamMemberTests.cs
@@ -30,7 +30,7 @@ namespace HelloWorldWeb.Tests
             teamService.RemoveMember(2);
 
             // Assert
-            Assert.Equal(6, teamService.GetTeamInfo().TeamMembers.Count);
+            Assert.Equal(7, teamService.GetTeamInfo().TeamMembers.Count);
         }
 
         [Fact]
@@ -45,6 +45,22 @@ namespace HelloWorldWeb.Tests
 
             // Assert
             Assert.Equal("UnitTest", teamService.GetTeamMemberById(memberId).Name);
+        }
+
+        [Fact]
+        public void CheckIdProblem()
+        {
+            //Assume
+            ITeamService teamService = new TeamService();
+            var memberToBeDeleted = teamService.GetTeamInfo().TeamMembers[teamService.GetTeamInfo().TeamMembers.Count-2];
+            var newMemberName = "Borys";
+            //Act
+            teamService.RemoveMember(memberToBeDeleted.Id);
+            var id = teamService.AddTeamMember(newMemberName);
+            teamService.RemoveMember(id);
+            //Assert
+            var member = teamService.GetTeamInfo().TeamMembers.Find(element => element.Name == "Borys");
+            Assert.Null(member);
         }
     }
 }
