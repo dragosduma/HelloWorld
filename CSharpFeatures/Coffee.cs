56fcf2c57d3a72eec302002fb66a1a0bae9b149b
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-19 10:57:27 +0300

    Created coffee method to learn about callings

---
+++ CSharpFeatures/Coffee.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpFeatures
+{
+    internal class Coffee
+    {
+        public string CoffeType { get; set; }
+        public Coffee()
+        {
+        }
+
+        public Coffee(string type)
+        {
+            CoffeType = type;
+        }
+
+        public override string ToString()
+        {
+            return CoffeType;
+        }
+    }
+}
--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -18,6 +18,40 @@ namespace CSharpFeatures
             var expectedOutput = readText.Result;
             var teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(expectedOutput);
             Console.WriteLine(teamMemberDeserialized);
+
+            Console.Write("What would you like? ");
+            var customerInput = Console.ReadLine();
+            Func<string, string, string, string, Coffee> recipe = (customerInput == "FlatWhite")? FlatWhite : Espresso;
+            
+            Coffee coffee = MakeCoffee("grain","milk","water","sugar", recipe);
+            Console.WriteLine($"Here is your coffee: {coffee}.");
+        }
+
+        static Coffee MakeCoffee(string grains, string milk, string water, string sugar, Func<string, string, string, string, Coffee> recipe)
+        {
+            try
+            {
+                Console.WriteLine("Start preparing coffee.");
+                var coffee = recipe(grains, milk, water, sugar);
+                return coffee;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Finished.");
+            }
+        }
+
+        static Coffee Espresso(string grains, string milk, string water, string sugar)
+        {
+            return new Coffee("Espresso");
+        }
+        static Coffee FlatWhite(string grains, string milk, string water, string sugar)
+        {
+            return new Coffee("FlatWhite");
         }
     }
 }
