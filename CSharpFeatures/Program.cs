483b7df72b6bdcc9f3fc76090f56e84f71ab90d4
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-19 09:46:16 +0300

    Added project for feature implementations

---
+++ CSharpFeatures/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TeamMember teamMember = new TeamMember() { Name = "Member1" };
+            string jsonString = JsonSerializer.Serialize(teamMember);
+            Console.WriteLine(jsonString);
+            File.WriteAllText("TeamMember.json", jsonString);
+            var teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(jsonString);
+            
+        }
+    }
+}
