dc06647130a3c59cc16addda2fa9571edb4da1d2
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-19 11:37:05 +0300

    Implement exceptions and try catch block

--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -9,7 +9,7 @@ namespace CSharpFeatures
         static void Main(string[] args)
         {
             
-            TeamMember teamMember = new TeamMember() { Name = "Member1" };
+           /* TeamMember teamMember = new TeamMember() { Name = "Member1" };
             string jsonString = JsonSerializer.Serialize(teamMember);
             Console.WriteLine(jsonString);
             File.WriteAllText("TeamMember.json", jsonString);
@@ -17,37 +17,51 @@ namespace CSharpFeatures
             readText.Wait();
             var expectedOutput = readText.Result;
             var teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(expectedOutput);
-            Console.WriteLine(teamMemberDeserialized);
+            Console.WriteLine(teamMemberDeserialized);*/
 
             Console.Write("What would you like? ");
             var customerInput = Console.ReadLine();
             Func<string, string, string, string, Coffee> recipe = (customerInput == "FlatWhite")? FlatWhite : Espresso;
             
             Coffee coffee = MakeCoffee("grain","milk","water","sugar", recipe);
-            Console.WriteLine($"Here is your coffee: {coffee}.");
+            if (coffee == null)
+            {
+                Console.WriteLine("Sorry, your order can't be completed");
+            }
+            else 
+            { 
+                Console.WriteLine($"Here is your coffee: {coffee}."); 
+            }
         }
 
         static Coffee MakeCoffee(string grains, string milk, string water, string sugar, Func<string, string, string, string, Coffee> recipe)
         {
+            Coffee coffee = null;
             try
             {
                 Console.WriteLine("Start preparing coffee.");
-                var coffee = recipe(grains, milk, water, sugar);
+                coffee = recipe(grains, milk, water, sugar);
                 return coffee;
             }
-            catch
+            catch (RecipeUnavailableException e)
             {
-                throw;
+                Console.WriteLine($"Something went wrong: {e.Message}.");
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something went wrong, see exception details: {e.Message}");
             }
             finally
             {
                 Console.WriteLine("Finished.");
             }
+            return coffee;
         }
 
         static Coffee Espresso(string grains, string milk, string water, string sugar)
         {
-            return new Coffee("Espresso");
+            throw new RecipeUnavailableException();
         }
         static Coffee FlatWhite(string grains, string milk, string water, string sugar)
         {
---
+++ CSharpFeatures/RecipeUnavailableException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFeatures
+{
+  
+    [Serializable]
+    public class RecipeUnavailableException : Exception
+    {
+        public RecipeUnavailableException() { }
+        public RecipeUnavailableException(string message) : base(message) { }
+        public RecipeUnavailableException(string message, Exception inner) : base(message, inner) { }
+        protected RecipeUnavailableException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
