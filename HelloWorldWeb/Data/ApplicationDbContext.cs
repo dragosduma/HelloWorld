3d6ee7bcf7a4118f9c3e37f8a1aed2dd8df6c879
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-08-26 12:42:04 +0300

    Implemented users page

--- HelloWorldWeb/Data/ApplicationDbContext.cs
+++ HelloWorldWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HelloWorldWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HelloWorldWeb.Data
---
+++ HelloWorldWeb/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldWeb.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Roles { get; set; }
+    }
+}
